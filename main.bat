6ed467c5c0e75be7e699fe0ee4a33af78cfa0d8f
LucasT-UC <lucastb@uc.cl>
2024-01-03 18:10:50 -0300

    Adding Ig, X, TikTok Functions

--- main.bat
+++ main.bat
@@ -1,13 +1,52 @@
 @ECHO off
 
+ECHO Welcome to MEDIADOWN
+ECHO ---------------------
+
+ECHO First you must choose the social media:
+ECHO 1. YOUTUBE
+ECHO 2. TWITTER
+ECHO 3. TIKTOK
+ECHO 4. INSTAGRAM
+
+SET /p "mode=Choose which option: "
+
 SET /p "a_v=Download audio or video ('a' or 'v'): "
 
 SET /p "url=Enter URL: "
 
-ECHO %url%
-ECHO %a_v%
+CLS
 
+IF %mode% == 1 (GOTO YoutubeDown)
+IF %mode% == 2 (GOTO TwitterDown)
+IF %mode% == 3 (GOTO TiktokDown)
+IF %mode% == 4 (GOTO InstagramDown) ELSE (GOTO Exit)
+
+
+:YoutubeDown
+ECHO DOWNLOADING YOUTUBE URL
 IF %a_v% EQU a (yt-dlp.exe -P "/Output" -x %url%)
 IF %a_v% EQU v (yt-dlp.exe -P "/Output" -w %url%)
+GOTO Exit
 
+:TwitterDown
+ECHO DOWNLOADING TWITTER URL
+IF %a_v% EQU a (yt-dlp.exe -P "/Output" -x %url%)
+IF %a_v% EQU v (yt-dlp.exe -P "/Output" -w %url%)
+GOTO Exit
+
+:TiktokDown
+ECHO DOWNLOADING TIKTOK URL
+IF %a_v% EQU a (yt-dlp.exe -P "/Output" -x %url%)
+IF %a_v% EQU v (yt-dlp.exe -P "/Output" -w %url%)
+GOTO Exit
+
+:InstagramDown
+ECHO DOWNLOADING INSTAGRAM URL
+IF %a_v% EQU a (yt-dlp.exe -P "/Output" -x %url%)
+IF %a_v% EQU v (yt-dlp.exe -P "/Output" -w %url%)
+GOTO Exit
+
+
+:Exit
 PAUSE
